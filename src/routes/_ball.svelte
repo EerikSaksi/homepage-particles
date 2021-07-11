a8a60c790ec76ba277e76f35e3b324f5d5c74240
Eerik Saksi <saksi.eerik@gmail.com>
2021-07-11 11:40:41 +0100

    adding intersection observer

---
+++ src/routes/_ball.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import IntersectionObserver from "svelte-intersection-observer";
+  let element;
+  let intersecting;
+
+  export let src: string;
+  export let selected: boolean;
+  export let onPress: () => void;
+
+</script>
+
+<IntersectionObserver>
+<button
+  on:click={onPress}
+  on:focus={onPress}
+  bind:this={element}
+  class="h-48 w-48 ml-12 md:ml-12 flex-shrink-0 focus:outline-none rounded-full flex justify-center items-center bg-white m-3 overflow-hidden shadow-md p-8 {selected
+    ? 'ring-4 transition-transform scale-110'
+    : ''}"
+>
+  <img alt="Noice" {src} />
+</button>
+  </IntersectionObserver>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Ball from "./_ball.svelte";
+  let selectedBall = 0
+</script>
+
+<div class="h-screen w-screen md:container md:mx-auto flex items-center md:justify-center">
+  <div class="flex flex-row md:justify-center overflow-y-hidden p-3">
+    <Ball src="https://upload.wikimedia.org/wikipedia/commons/4/41/Uros-logo-plain.png" selected = {selectedBall === 0} onPress = {() => {selectedBall = 0}}/>
+    <Ball src="https://idsihealth.org/wp-content/uploads/2015/01/University-of-Glasgow.jpg" selected = {selectedBall === 1} onPress = {() => {selectedBall = 1}}/>
+    <Ball src="https://brokerchooser.com/uploads/broker_logos/barclays-review.png" selected = {selectedBall === 2}  onPress = {() => {selectedBall = 2}}/>
+  </div>
+</div>
