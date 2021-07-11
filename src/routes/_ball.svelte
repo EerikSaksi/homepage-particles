d3a00fc882317a792fae70b565e008b15dd48d13
Eerik Saksi <saksi.eerik@gmail.com>
2021-07-11 11:52:55 +0100

    Focus only on mobile now works

--- src/routes/_ball.svelte
+++ src/routes/_ball.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
   import IntersectionObserver from "svelte-intersection-observer";
-  let element;
-  let intersecting;
+  import MediaQuery from "svelte-media-query";
+  let element: HTMLElement;
 
   export let src: string;
   export let selected: boolean;
   export let onPress: () => void;
-
 </script>
 
-<IntersectionObserver>
-<button
-  on:click={onPress}
-  on:focus={onPress}
-  bind:this={element}
-  class="h-48 w-48 ml-12 md:ml-12 flex-shrink-0 focus:outline-none rounded-full flex justify-center items-center bg-white m-3 overflow-hidden shadow-md p-8 {selected
-    ? 'ring-4 transition-transform scale-110'
-    : ''}"
->
-  <img alt="Noice" {src} />
-</button>
+<MediaQuery query="(min-width: 768px)" let:matches>
+  <IntersectionObserver
+    {element}
+    threshold={1}
+    once={matches}
+    on:intersect={() => {
+      if (!matches) {
+        onPress()
+      }
+    }}
+  >
+    <button
+      on:click={onPress}
+      on:focus={onPress}
+      bind:this={element}
+      class="h-48 w-48 ml-12 md:ml-12 flex-shrink-0 focus:outline-none rounded-full flex justify-center items-center bg-white m-3 overflow-hidden shadow-md p-8 {selected
+        ? 'ring-4 transition-transform scale-110'
+        : ''}"
+    >
+      <img alt="Noice" {src} />
+    </button>
   </IntersectionObserver>
+</MediaQuery>
